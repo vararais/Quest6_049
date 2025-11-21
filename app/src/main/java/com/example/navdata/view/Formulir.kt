49d992c63d25eb4dc8edb9be65bf8b87aaffdc5e
vararais <varadigmaisnann@gmail.com>
2025-11-21 15:36:34 +0700

    Mengubah Row untuk pemilihan gender

--- app/src/main/java/com/example/navdata/view/Formulir.kt
+++ app/src/main/java/com/example/navdata/view/Formulir.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -78,11 +79,19 @@ fun FormIsian(
                     .width(250.dp), thickness = Thickness, color = Color.Red
             )
             Row {
-                jenisK.forEach { item ->
-                    Row(verticalAlignment = Alignment.CenterVertically) {
+                pilihanJK.forEach { item ->
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender == item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+                        verticalAlignment = Alignment.CenterVertically) {
                         RadioButton(
-                            selected = false,
-                            onClick = {item}
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
                         )
                         Text(text = item)
                     }
