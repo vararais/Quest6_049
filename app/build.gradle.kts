0497d236e5a5938ac62d286f8df4935e404d72fc
vararais <varadigmaisnann@gmail.com>
2025-11-21 15:06:19 +0700

    Menambah implementation untuk view models

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.ui)
