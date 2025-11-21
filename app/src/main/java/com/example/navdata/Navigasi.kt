b178e87d45862b39569abe77047813cac380e360
vararais <varadigmaisnann@gmail.com>
2025-11-21 14:48:46 +0700

    Mengimpor compose

--- app/src/main/java/com/example/navdata/Navigasi.kt
+++ app/src/main/java/com/example/navdata/Navigasi.kt
@@ -1,5 +1,14 @@
 package com.example.navdata
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 
 enum class Navigasi {
     Formulirku,
