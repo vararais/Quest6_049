dc2b1bda11e391abb8fbaa1f9238eb74b6a46d09
vararais <varadigmaisnann@gmail.com>
2025-11-21 14:47:49 +0700

    Menambahkan file copy paste Navigasi

--- app/src/main/java/com/example/navdata/Navigasi.kt
+++ app/src/main/java/com/example/navdata/Navigasi.kt
@@ -1,4 +1,41 @@
 package com.example.navdata
 
 
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
 
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian (
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData (
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+){
+    navController.popBackStack(Navigasi.Formulirku.name, inclusive = false)
+}
