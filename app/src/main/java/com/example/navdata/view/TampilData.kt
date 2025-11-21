3fe838f452adf9096738c3a0f9c981d34f9b4007
vararais <varadigmaisnann@gmail.com>
2025-11-21 16:03:04 +0700

    Mengumbah tampildata

--- app/src/main/java/com/example/navdata/view/TampilData.kt
+++ app/src/main/java/com/example/navdata/view/TampilData.kt
@@ -24,16 +24,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.navdata.R
+import com.example.navdata.model.Siswa
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
-    onBackBtnClick: () -> Unit
+    statusUiSiswa: Siswa,
+    onBackButtonClicked:()-> Unit
 ){
     val items = listOf(
-        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
-        Pair(stringResource(id = R.string.jenis_kelamin), "Yang Lain"),
-        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+        Pair(stringResource(id = R.string.nama), statusUiSiswa.nama),
+        Pair(stringResource(id = R.string.gender), statusUiSiswa.gender),
+        Pair(stringResource(id = R.string.alamat), statusUiSiswa.alamat)
     )
     Scaffold (modifier = Modifier,
         {
@@ -56,7 +58,7 @@ fun TampilData(
                 Spacer(modifier = Modifier.height(10.dp))
                 Button(
                     modifier = Modifier.fillMaxWidth(),
-                    onClick = onBackBtnClick) {
+                    onClick = onBackButtonClicked) {
                     Text(stringResource(id = R.string.back))
                 }
             }
